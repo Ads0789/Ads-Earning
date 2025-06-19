2928f52f8f6c121e259e18b48afafa27368dbbb9
Ads <das07142@gmail.com>
2025-06-19 13:43:50 +0530

    add

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1niagsjTF1bzmXeTSpb-WVGoWF-hqLRo",
+  authDomain: "tasktriumph.firebaseapp.com",
+  projectId: "tasktriumph",
+  storageBucket: "tasktriumph.firebasestorage.app",
+  messagingSenderId: "851279586148",
+  appId: "1:851279586148:web:5331480f40753b9207d071"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
